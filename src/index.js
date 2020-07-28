35509516fdd2eb5aa7519ff114b7b13dc253814b
xuhaomei <xuhaomei@pcitech.com>
2020-07-28 13:42:54 +0800

    初始化项目

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function component () {
+    let div = document.createElement('div');
+    div.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return div;
+}
+
+document.body.appendChild(component());
